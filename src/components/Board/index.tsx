64c23de6403d0e6a94f656abf570784865950695
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-02-13 18:07:39 -0300

    verificando vencerdor

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -1,5 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BoxBorard, Span } from "./styled";
+
+const winningCombinations = [
+  // horizontais
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+
+  // verticals
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+
+  //diagonals
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 export function Board() {
   const [gameData, setGameData] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
@@ -20,6 +36,37 @@ export function Board() {
 
     setTurn((prev) => (prev === 1 ? 2 : 1));
   };
+
+  useEffect(() => {
+    checkWinner();
+  }, [gameData]);
+
+  const checkWinner = () => {
+    console.log("Winner");
+
+    let winner = null;
+
+    for (let values of winningCombinations) {
+      if (
+        gameData[values[0]] === 1 &&
+        gameData[values[1]] === 1 &&
+        gameData[values[2]] === 1
+      ) {
+        winner = "player 1";
+      }
+
+      if (
+        gameData[values[0]] === 2 &&
+        gameData[values[1]] === 2 &&
+        gameData[values[2]] === 2
+      ) {
+        winner = "player 2";
+      }
+    }
+
+    console.log({ winner });
+  };
+
   return (
     <BoxBorard>
       {gameData.map((value, index) => (
@@ -27,6 +74,7 @@ export function Board() {
           onClick={() => {
             handleClick(index);
           }}
+          key={index}
         >
           {value === 1 && "x"}
           {value === 2 && "o"}
--- src/components/Board/styled.ts
+++ src/components/Board/styled.ts
@@ -12,12 +12,14 @@ export const BoxBorard = styled.div`
 
 export const Span = styled.span`
   width: 33.33333%;
+
   display: flex;
   align-items: center;
   justify-content: center;
   color: #333;
   font-size: 70px;
   font-weight: bold;
+  outline: 1px solid #ccc;
 
   &:hover {
     background: #f6f6f6;
